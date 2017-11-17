cfd3093cff9313382f633269d1a7f175f480c6e0
Xiltyn <xiltyn@gmail.com>
2017-11-17 19:04:35 +0100

    tweaking scripts

--- prodServer.js
+++ prodServer.js
@@ -7,10 +7,10 @@ const app = express();
 const compiler = webpack(config);
 const PORT = process.env.PORT || 3010;
 
-// app.use(require('webpack-dev-middleware')(compiler, {
-// 	noInfo: true,
-// 	publicPath: config.output.publicPath
-// }));
+app.use(require('webpack-dev-middleware')(compiler, {
+	noInfo: true,
+	publicPath: config.output.publicPath
+}));
 
 app.use('/assets', express.static('./assets'));
 
